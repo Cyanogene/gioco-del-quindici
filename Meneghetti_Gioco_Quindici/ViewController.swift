24670cefa9e0d8440d485088235ffd9262879a28
Unknown <matteo.meneghetti02@gmail.com>
2019-11-15 09:01:29 +0100

    Generatore funzionante

--- Meneghetti_Gioco_Quindici/ViewController.swift
+++ Meneghetti_Gioco_Quindici/ViewController.swift
@@ -11,25 +11,52 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button10: UIButton!
+    @IBOutlet weak var button11: UIButton!
+    @IBOutlet weak var button12: UIButton!
+    @IBOutlet weak var button13: UIButton!
+    @IBOutlet weak var button14: UIButton!
+    @IBOutlet weak var button15: UIButton!
     @IBOutlet weak var buttonEmpty: UIButton!
-    @IBOutlet var button: [UIButton]!
 
-    var matrice : [[Int]] = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
+
+    var matrice : [[Int]] = [[]]
+    var matriceButtons : [[UIButton]] = [[]]
     
      override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    @IBAction func btn_1(_ sender: UIButton)
-    {
-        
-    }
-
-    @IBAction func btn_14(_ sender: UIButton) {
-                                         
+        matrice = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
+        matriceButtons = [[button1,button2,button3,button4],[button5,button6,button7,button8],[button9,button10,button11,button12],[button13,button14,button15,buttonEmpty]]
     }
     
-     func generatore() {
+    @IBAction func btn_creaPartita(_ sender: UIButton) {
+        generatore()
+        var x = 0
+        var y = 0
+        
+        
+        for _ in 0...3 {
+            for _ in 0...3 {
+                matriceButtons[x][y].setTitle(String(matrice[x][y]), for: .normal)
+                y = y + 1
+            }
+            x = x + 1
+            y = 0
+        }
+ 
+        
+        
+    }
+    func generatore() {
          let number = Int.random(in: 10 ... 21)
          var x = 3
          var y = 3
@@ -62,7 +89,7 @@ class ViewController: UIViewController {
                     matrice[x][y] = 0;
                 }
 
-                if (rand1 < rand2 || rand1 % 2 != 0)
+                else
                 {
                     matrice[x][y] = matrice[x][y - 1]
                     y = y - 1;
